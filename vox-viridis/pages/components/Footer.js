a8bfd8922ee771b5c946717f09910ce4f5de5536
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-09-24 18:00:47 +0800

    added basic background for frontend

---
+++ vox-viridis/pages/components/Footer.js
@@ -0,0 +1,17 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <a
+                href="https://franky-lim24.github.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+
+            >
+                Powered by Franky Lim
+
+            </a>
+        </footer >
+    )
+}
---
+++ vox-viridis/pages/components/Home/Homepage.js
@@ -0,0 +1,13 @@
+import styles from './Homepage.module.css'
+
+function Homepage() {
+    return (
+        <div className={styles.Homepage}>
+            
+        </div>
+    );
+}
+
+
+
+export default Homepage;
