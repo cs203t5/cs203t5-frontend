96e8ec00c2d4c52b83c56ded8f96ae5c3bd3bbd0
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-10-02 01:22:38 +0800

    completed campaign page

--- vox-viridis/pages/_app.js
+++ vox-viridis/pages/_app.js
@@ -1,12 +1,30 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
         import("bootstrap/dist/js/bootstrap.bundle.min");
     }, []);
-    return <Component {...pageProps} />;
+    return (
+        <div className="">
+            <Head>
+                {" "}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Italiana&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />;
+        </div>
+    );
 }
 
 export default MyApp;
