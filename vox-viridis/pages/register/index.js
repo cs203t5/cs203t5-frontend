5dfa0fa6264d343889209480b353a1811a66d225
Jun Hui <junhui.tan.2021@scis.smu.edu.sg>
2022-10-02 01:37:03 +0800

    registration page

---
+++ vox-viridis/pages/register/index.js
@@ -0,0 +1,156 @@
+import globalStyle from "../Global.module.css";
+
+const Register = () => {
+    return (
+        <div>
+            <div className = {globalStyle.pageBg}>
+                <div className = "row mt-6">
+                    <div className = "col lg-12">
+                        <img 
+                        src = "../../register/registration.svg"
+                        className = "img-fluid w-50 mt-5">
+                        </img>
+                    </div>
+                </div>
+
+                <div className = "col lg-12"
+                style = {{
+                    position : "relative",
+                    fontSize : "50px",
+                    left : "1000px",
+                    bottom : "650px"
+                }}>
+                    Sign Up
+                </div>
+
+                <div className = "col lg-12"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    left : "1000px",
+                    bottom : "600px"
+                }}>
+                    First Name
+                    <div 
+                className = "card text-white"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    width : "300px",
+                }}>
+                    TEST
+                    </div>
+                </div>
+
+                <div className = "col lg-12"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    left : "1400px",
+                    bottom : "695px"
+                }}>
+                    Last Name
+                    <div 
+                className = "card text-white"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    width : "300px",
+                }}>
+                    TEST
+                    </div>
+                </div>
+
+                <div className = "col lg-12"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    left : "1000px",
+                    bottom : "650px"
+                }}>
+                    Username
+                    <div 
+                className = "card text-white"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    width : "700px",
+                }}>
+                    TEST
+                    </div>
+                </div>
+
+                <div className = "col lg-12"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    left : "1000px",
+                    bottom : "600px"
+                }}>
+                    Email
+                    <div 
+                className = "card text-white"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    width : "700px",
+                }}>
+                    TEST
+                    </div>
+                </div>
+
+                <div className = "col lg-12"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    left : "1000px",
+                    bottom : "550px"
+                }}>
+                    Password
+                    <div 
+                className = "card text-white"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    width : "700px",
+                }}>
+                    TEST
+                    </div>
+                </div>
+
+                <div className = "col lg-12"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    left : "1000px",
+                    bottom : "500px"
+                }}>
+                    Date of Birth
+                    <div 
+                className = "card text-white"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    width : "700px",
+                }}>
+                    TEST
+                    </div>
+                </div>
+
+                <div 
+                className = "card text-black bg-success mb-3"
+                style = {{
+                    position : "relative",
+                    fontSize : "30px",
+                    width : "200px",
+                    left : "1000px",
+                    bottom : "400px"
+                }}>
+                    Register
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
