3a015c2312056ab1c4547f833904049259961e6c
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-10-02 13:54:17 +0800

    made login

--- vox-viridis/components/Navbar.js
+++ vox-viridis/components/Navbar.js
@@ -4,17 +4,15 @@ import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import Link from "next/link";
 import Nav from "react-bootstrap/Nav";
 import NavbarTemplate from "react-bootstrap/Navbar";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Navbar = () => {
     return (
         <div className="container-fluid p-0">
             <div className="row m-auto">
                 <div className="col ms-auto p-0">
-                    <Link href="/login">
-                        <div
-                            className="col float-end me-3 mt-1"
-                            style={{ cursor: "pointer" }}
-                        >
+                    <Dropdown>
+                        <Dropdown.Toggle className="col float-end me-3 mt-1 dropdown">
                             <AccountCircleRoundedIcon
                                 style={{ fontSize: "xx-large" }}
                             />
@@ -25,8 +23,62 @@ const Navbar = () => {
                             >
                                 Login
                             </span>
-                        </div>
-                    </Link>
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <form className="px-4 py-1">
+                                <div className="form-group">
+                                    <label htmlFor="exampleDropdownFormEmail1">
+                                        Email address
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        // id="exampleDropdownFormEmail1"
+                                        placeholder="email@example.com"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleDropdownFormPassword1">
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        // id="exampleDropdownFormPassword1"
+                                        placeholder="Password"
+                                    />
+                                </div>
+                                <div className="form-check mt-1">
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input"
+                                        // id="dropdownCheck"
+                                    />
+                                    <label
+                                        className="form-check-label"
+                                        htmlFor="dropdownCheck"
+                                    >
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary"
+                                    style={{ marginTop: "5px" }}
+                                >
+                                    Sign in
+                                </button>
+                            </form>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="/register">
+                                New around here? Sign up
+                            </a>
+                            <a className="dropdown-item" href="/forgetpassword">
+                                Forgot password?
+                            </a>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
             </div>
 
