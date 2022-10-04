9e388047dbec85860549942e31822a9394ccdd88
Shao Dong <sdlee.2021@scis.smu.edu.sg>
2022-10-04 22:26:26 +0800

    experimented with help page

--- vox-viridis/pages/help/index.js
+++ vox-viridis/pages/help/index.js
@@ -1,10 +1,30 @@
-import Navbar from "../../components/Navbar";
 import globalStyle from "../Global.module.css";
+import Navbar from "../../components/Navbar";
+import NavBarLogin from "../../components/NavBarLogin";
+
+const Navbar2 = () => {
+    return (
+        <div class="container-fluid border text-center">
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <img src="../../vox Icon.jpg" width="190" height="85" />
+                </div>
+                <div class="col-4">
+                    <NavBarLogin />
+                </div>
+            </div>
+
+        </div>
+        
+    );
+};
+
 const index = () => {
     return (
         <div>
             <div className={globalStyle.pageBg}>
-                <Navbar />
+                <Navbar2 />
             </div>
         </div>
     );
