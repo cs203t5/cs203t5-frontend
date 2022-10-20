5c7747689d25fec701cad3acbfb264c2a51c0c88
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-10-21 04:01:45 +0800

    completed history

--- vox-viridis/pages/history/index.js
+++ vox-viridis/pages/history/index.js
@@ -3,13 +3,19 @@ import globalStyle from "../Global.module.css";
 
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import HistoryCard from "../../components/History/HistoryCard";
 
 function index() {
     return (
         <div>
             <div className={globalStyle.pageBg}>
                 <Navbar />
-                
+                <div className="row m-0 p-0 mx-5 my-5">
+                    <HistoryCard state={"ongoing"} />
+                    <HistoryCard state={"completed"} />
+                    <HistoryCard state={"upcoming"} />
+                    <HistoryCard state={"ongoing"} />
+                </div>
             </div>
         </div>
     );
