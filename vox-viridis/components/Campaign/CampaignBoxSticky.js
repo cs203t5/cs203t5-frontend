76b5bbd9e501c7d7e9016624a73e9b73a116576e
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-10-26 03:29:25 +0800

    added data validation for campaignBox

--- vox-viridis/components/Campaign/CampaignBoxSticky.js
+++ vox-viridis/components/Campaign/CampaignBoxSticky.js
@@ -82,99 +82,102 @@ function CampaignBoxSticky(props) {
                     </div>
                 </div>
             </div>
-            {props.campaignData.length === 2 && (
-                <div
-                    className="col-lg-5 md-5 col-xs-6  border border-dark "
-                    style={{
-                        display: "flex",
+            {props.campaignData.length === 2 &&
+                props.campaignData[1] === undefined && (
+                    <div
+                        className="col-lg-5 md-5 col-xs-6  border border-dark "
+                        style={{
+                            display: "flex",
 
-                        borderRadius: "10px",
-                        fontWeight: "500",
-                        fontSize: "0.9em",
-                        cursor: "pointer",
-                    }}
-                >
-                    <div className="container h-100">
-                        <div
-                            className="row w-100 "
-                            style={{ alignItems: "center" }}
-                        >
-                            <div className="col-3 p-0 h-25" style={{}}>
-                                <img
-                                    src={props.campaignData[1]?.image}
-                                    className="img-fluid w-75 mt-2 ms-2"
-                                    style={{
-                                        width: "60px",
-                                        height: "100px",
-                                        aspectRatio: "1",
-                                        alignSelf: "center",
-                                        objectFit: "contain" /*magic*/,
-                                    }}
-                                />
-                            </div>
+                            borderRadius: "10px",
+                            fontWeight: "500",
+                            fontSize: "0.9em",
+                            cursor: "pointer",
+                        }}
+                    >
+                        <div className="container h-100">
                             <div
-                                className="col-8 p-0 ms-2 h-25 mt-sm-3 mt-2 text-align-center"
-                                style={{ fontWeight: "600" }}
+                                className="row w-100 "
+                                style={{ alignItems: "center" }}
                             >
-                                {" "}
-                                {props.campaignData[1]?.title}
-                                <div style={{ color: "#596D55" }}>
-                                    {props.campaignData[1]?.companyName}
+                                <div className="col-3 p-0 h-25" style={{}}>
+                                    <img
+                                        src={props.campaignData[1]?.image}
+                                        className="img-fluid w-75 mt-2 ms-2"
+                                        style={{
+                                            width: "60px",
+                                            height: "100px",
+                                            aspectRatio: "1",
+                                            alignSelf: "center",
+                                            objectFit: "contain" /*magic*/,
+                                        }}
+                                    />
                                 </div>
-                                <br />
+                                <div
+                                    className="col-8 p-0 ms-2 h-25 mt-sm-3 mt-2 text-align-center"
+                                    style={{ fontWeight: "600" }}
+                                >
+                                    {" "}
+                                    {props.campaignData[1]?.title}
+                                    <div style={{ color: "#596D55" }}>
+                                        {props.campaignData[1]?.companyName}
+                                    </div>
+                                    <br />
+                                </div>
                             </div>
-                        </div>
-                        <div className="row w-100 mt-2">
-                            <div className="col-2 ms-4 p-0">
-                                <img
-                                    src="../../campaign/gift 1.svg"
-                                    className="img-fluid w-50 "
-                                />
+                            <div className="row w-100 mt-2">
+                                <div className="col-2 ms-4 p-0">
+                                    <img
+                                        src="../../campaign/gift 1.svg"
+                                        className="img-fluid w-50 "
+                                    />
+                                </div>
+                                <div
+                                    className="col-8"
+                                    style={{ alignSelf: "center" }}
+                                >
+                                    Free Face Mask
+                                </div>
                             </div>
-                            <div
-                                className="col-8"
-                                style={{ alignSelf: "center" }}
-                            >
-                                Free Face Mask
+                            <div className="row w-100 mt-2">
+                                <div className="col-2 ms-4 p-0">
+                                    <img
+                                        src="../../campaign/location 1.svg"
+                                        className="img-fluid w-50 "
+                                    />
+                                </div>
+                                <div
+                                    className="col-8"
+                                    style={{ alignSelf: "center" }}
+                                >
+                                    {props.campaignData[1]?.address}
+                                </div>
                             </div>
-                        </div>
-                        <div className="row w-100 mt-2">
-                            <div className="col-2 ms-4 p-0">
-                                <img
-                                    src="../../campaign/location 1.svg"
-                                    className="img-fluid w-50 "
-                                />
-                            </div>
-                            <div
-                                className="col-8"
-                                style={{ alignSelf: "center" }}
-                            >
-                                {props.campaignData[1]?.address}
-                            </div>
-                        </div>
-                        <div className="row w-100 mt-2 mb-3">
-                            <div className="col-2 ms-4 p-0">
-                                <img
-                                    src="../../campaign/clock 1.svg"
-                                    className="img-fluid w-50 "
-                                />
-                            </div>
-                            <div
-                                className="col-8"
-                                style={{ alignSelf: "center" }}
-                            >
-                                {`${
-                                    props.campaignData[1]?.startDate.split(
-                                        "T"
-                                    )[0]
-                                } - ${
-                                    props.campaignData[1]?.endDate.split("T")[0]
-                                }`}
+                            <div className="row w-100 mt-2 mb-3">
+                                <div className="col-2 ms-4 p-0">
+                                    <img
+                                        src="../../campaign/clock 1.svg"
+                                        className="img-fluid w-50 "
+                                    />
+                                </div>
+                                <div
+                                    className="col-8"
+                                    style={{ alignSelf: "center" }}
+                                >
+                                    {`${
+                                        props.campaignData[1]?.startDate.split(
+                                            "T"
+                                        )[0]
+                                    } - ${
+                                        props.campaignData[1]?.endDate.split(
+                                            "T"
+                                        )[0]
+                                    }`}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            )}
+                )}
         </div>
     );
 }
