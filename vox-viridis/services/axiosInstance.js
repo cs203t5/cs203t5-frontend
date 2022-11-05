e5f199ff59b81f521bacbf1613395215d9ec9d3d
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2022-11-05 22:15:35 +0800

    Update axiosInstance.js

--- vox-viridis/services/axiosInstance.js
+++ vox-viridis/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Instance = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "http://13.212.138.130/api/",
 });
 
 export default Instance;
