e2eb39af3ec38c58c3d2c03885bf35e7cba23e26
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-11-08 14:37:52 +0800

    test

--- vox-viridis/components/Campaign/CampaignBoxSticky.js
+++ vox-viridis/components/Campaign/CampaignBoxSticky.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 
 function CampaignBoxSticky(props) {
-    console.log(props.campaignData[0].rewards.rewardName);
+    console.log(props.campaignData[0]?.rewards?.rewardName);
+    console.log(props.campaignData[1]?.rewards?.rewardName);
     return (
         <div className="row p-0 mb-5 gap-5 mx-3">
             <div
@@ -75,7 +76,11 @@ function CampaignBoxSticky(props) {
                                 className="col-8"
                                 style={{ alignSelf: "center" }}
                             >
-                                Free Face Mask
+                                {props.campaignData[0]?.rewards?.rewardName ===
+                                null
+                                    ? "Free Face Mask"
+                                    : props.campaignData[0]?.rewards
+                                          ?.rewardName}
                             </div>
                         </div>
                         <div className="row w-100 mt-2">
@@ -202,7 +207,11 @@ function CampaignBoxSticky(props) {
                                         className="col-8"
                                         style={{ alignSelf: "center" }}
                                     >
-                                        Free Face Mask
+                                        {props.campaignData[1]?.rewards
+                                            ?.rewardName === null
+                                            ? "Free Face Mask"
+                                            : props.campaignData[1]?.rewards
+                                                  ?.rewardName}
                                     </div>
                                 </div>
                                 <div className="row w-100 mt-2">
